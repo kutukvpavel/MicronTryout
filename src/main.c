cd9d0f0b85b7f6c28e70feacd28b495ae3b46f5a
paul <paul@MAINPC>
2024-12-16 14:26:59 +0300

    Add SPI, timers and new SCR1 initialization

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 
 soft_timer led_timer = { .interval = 1000000 };
 soft_timer cli_timer = { .interval = 5000 };
+soft_timer spi_timer = { .interval = 12000 };
 
 int main()
 { 
@@ -16,15 +17,31 @@ int main()
     while (1)
     {
         wdt_reset();
+        if (check_soft_timer(&spi_timer))
+        {
+            static uint16_t duty0 = 0;
+            static uint16_t duty1 = PWM_TOP;
+
+            spi_dummy_transmit();
+            set_pwm_duty(MOTOR_MAIN_0, duty0);
+            set_pwm_duty(MOTOR_MAIN_1, duty1);
+            duty0 += 10;
+            if (duty0 >= PWM_TOP) duty0 = 0;
+            duty1 -= 10;
+            if (duty1 <= 0) duty1 = PWM_TOP;
+        }
         if (check_soft_timer(&led_timer))
         {
+            //static float dummy = 0;
+
             toggle_green_led();
+            //xprintf("Tick %.2f\n", dummy);
+            //dummy += 1.5f;
         }
         if (check_soft_timer(&cli_timer))
         {
             cli_run();
         }
-        delay_ms(1);
     }
     __unreachable();
 }
--- src/my_hal.h
+++ src/my_hal.h
@@ -9,15 +9,29 @@
 #include <mik32_hal_wdt.h>
 #include <uart.h>
 
+#define ENABLE_WDT 1
+
+#define PWM_TOP 16000
+
 #define TIMER_MICROS TIMER32_2
 #define UART_STDOUT UART_1
+#define UART_STDOUT_EPIC_MASK EPIC_UART_1_INDEX
 #define UART_STDOUT_EPIC_CHECK() EPIC_CHECK_UART_1()
 #define UART_BUF_SIZE 128
 
+#define HAL_ASSERTION_FAILED 0x04
+
 #define _BV(bit) (1u << (bit))
+#define RAM_ATTR __attribute__( ( noinline, section(".ram_text") ) )
 
 //Public API
 
+enum
+{
+    MOTOR_MAIN_0,
+    MOTOR_MAIN_1,
+    MOTOR_AUX
+} typedef motor_t;
 struct _soft_timer
 {
     uint32_t interval; //us
@@ -28,6 +42,8 @@ HAL_StatusTypeDef my_hal_init(void);
 void delay_us(uint32_t us);
 void toggle_red_led(void);
 void toggle_green_led(void);
+HAL_StatusTypeDef spi_dummy_transmit(void);
+HAL_StatusTypeDef set_pwm_duty(motor_t ch, uint16_t duty);
 
 extern inline uint32_t get_micros(void);
 extern inline uint32_t get_time_past(uint32_t from);
